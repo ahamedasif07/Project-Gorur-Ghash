454b5ecf219b32d7bbe7fba641517f487e7e78f1
rxasif07 <rxasif31@gmail.com>
2024-09-29 13:08:57 +0700

    paths sent by props

--- src/components/Home/Catagories.js
+++ src/components/Home/Catagories.js
@@ -3,11 +3,10 @@ import Image from "next/image";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
 import React from "react";
-import catImg from "../../asstes/gorur-ghash-home/10.png";
+import Link from "next/link";
 
-const Catagories = ({ image1, image2 }) => {
+const Catagories = ({ image1, image2, path1, path2 }) => {
   useEffect(() => {
     AOS.init({
       duration: 900, // Animation duration
@@ -15,15 +14,16 @@ const Catagories = ({ image1, image2 }) => {
       once: true, // Whether animation should happen only once while scrolling
     });
   }, []);
+
   return (
     <div>
       <div className="flex md:flex-row flex-col overflow-hidden md:max-w-screen-lg px-4 justify-center pb-[20px] mx-auto gap-5">
-        <div data-aos="fade-right">
-          <Image src={image1} alt="image"></Image>
-        </div>
-        <div data-aos="fade-left">
-          <Image src={image2} alt="image"></Image>
-        </div>
+        <Link href={path1} data-aos="fade-right">
+          <Image src={image1} alt="Men's Category" width={500} height={300} />
+        </Link>
+        <Link href={path2} data-aos="fade-left">
+          <Image src={image2} alt="Women's Category" width={500} height={300} />
+        </Link>
       </div>
     </div>
   );
