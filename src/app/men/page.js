607d11fd5aaf868239aff172029a0fcfb170c049
rxasif07 <rxasif31@gmail.com>
2024-09-30 10:35:31 +0700

    mens catagory direct link done

--- src/app/men/page.js
+++ src/app/men/page.js
@@ -1,6 +1,64 @@
 import React from "react";
 
 const Men = () => {
+  const productCategories = [
+    {
+      category: "CO-ORD SET MALE",
+      products: 3,
+      image: "https://i.postimg.cc/wTVbdbsB/605-A8210-copy-300x300.jpg",
+    },
+    {
+      category: "POLOS",
+      products: 9,
+      image: "https://i.postimg.cc/FzhgWNpV/605-A5900-copy-300x300.jpg",
+    },
+    {
+      category: "SWEATSHIRTS",
+      products: 1,
+      image: "phttps://i.postimg.cc/8kwhk8wx/605-A4269-300x300.jpg",
+    },
+    {
+      category: "TANK TOPS",
+      products: 3,
+      image: "phttps://i.postimg.cc/c42t6d3k/605-A9260-300x300.jpg",
+    },
+    {
+      category: "SHIRT",
+      products: 71,
+      image: "https://i.postimg.cc/y6rkr7L6/605-A3172-copy-300x300.jpg",
+    },
+    {
+      category: "T-SHIRTS",
+      products: 26,
+      image: "https://i.postimg.cc/c4g6pPzR/605-A1845-copy-300x300.jpg",
+    },
+    {
+      category: "PANTS",
+      products: 26,
+      image: "phttps://i.postimg.cc/nrQZWfLM/605-A9338-copy-300x300.jpg",
+    },
+    {
+      category: "HOODIE BLACK",
+      products: 1,
+      image: "phttps://i.postimg.cc/tCWn1LzX/605-A9517-300x300.jpg",
+    },
+    {
+      category: "TURTLENECK",
+      products: 0, // Sample
+      image: "https://i.postimg.cc/y6Sk5wtF/605-A0352-300x300.jpg",
+    },
+    {
+      category: "HOODIE",
+      products: 0, // Sample
+      image: "https://i.postimg.cc/htMjTgQq/7-300x300.jpg",
+    },
+    {
+      category: "JACKET",
+      products: 0, // Sample
+      image: "phttps://i.postimg.cc/k5pgYcy1/605-A0352-1-300x300.jpg",
+    },
+  ];
+
   return <div></div>;
 };
 
