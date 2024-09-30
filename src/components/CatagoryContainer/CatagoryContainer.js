01ed9f9d546ea0f736baf092d97e5f5965311642
rxasif07 <rxasif31@gmail.com>
2024-09-30 20:12:35 +0700

    filtaring part desing done

--- src/components/CatagoryContainer/CatagoryContainer.js
+++ src/components/CatagoryContainer/CatagoryContainer.js
@@ -7,19 +7,38 @@ const CatagoryContainer = ({ productCategories }) => {
   return (
     <div>
       <Section>
-        <div className="flex md:flex-row flex-col gap-8 ">
-          <div className="md:w-1/3 flex gap-2 border-r-2  border-gray-600 mt-[15px] mx-auto md:mt-[50px]  ">
-            <input
-              className="border-2 py-2 h-[50px] px-4  border-black"
-              placeholder="search"
-              type="search"
-            />
-            <button className="px-3 py-2 hover:text-white hover:bg-black  h-[50px] transition-all duration-300 ease-in-out bg-transparent border-2 border-black">
-              Search
-            </button>
+        <div className="flex md:flex-row flex-col gap-12 ">
+          <div className="md:w-1/3  border-gray-800  border-r-2 md:my-[50px] my-[15px] ">
+            <div>
+              <div className="flex gap-2 px-6 ">
+                <input
+                  className="border-2 py-2 w-full h-[50px] px-4  border-black"
+                  placeholder="search"
+                  type="search"
+                />
+
+                <button className="px-3 py-2 hover:text-white hover:bg-black  h-[50px] transition-all duration-300 ease-in-out bg-transparent border-2 border-black">
+                  Search
+                </button>
+              </div>
+            </div>
+            <div>
+              <h2 className="text-3xl px-6 md:mt-7 mt-4 font-semibold text-gray-600">
+                Filter by price
+              </h2>
+            </div>
+            <div className="flex items-center  mt-2 px-6">
+              <p className="h-[14px] rounded-full w-[14px] bg-[#4E4E4E]"></p>
+              <p className="h-[4px]  w-[200px] bg-[#4E4E4E]"></p>
+              <p className="h-[14px] rounded-full w-[14px] bg-[#4E4E4E]"></p>
+            </div>
+            <div className="flex  gap-7 items-center  mt-2 px-6">
+              <p className="bg-[#FBDD02] py-1 px-2 rounded-md">Filter</p>
+              <p>price :$ 00 - $ 2500</p>
+            </div>
           </div>
           {/* ----------products catgory cardsm------------- */}
-          <div className="md:w-2/3 md:mt-[50px]  px-10 ">
+          <div className="md:w-2/3 md:mt-[50px]  px-6 ">
             <div className="product-catagory-card grid  md:grid-cols-4 gap-4 grid-cols-2">
               {productCategories.map((productCatagory, index) => (
                 <ProductCatagoryCard
