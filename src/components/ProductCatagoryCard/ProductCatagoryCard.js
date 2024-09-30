d11763826f727c967e4058b133f4c171ef84ebcd
rxasif07 <rxasif31@gmail.com>
2024-09-30 18:54:00 +0700

    men products catagory done

--- src/components/ProductCatagoryCard/ProductCatagoryCard.js
+++ src/components/ProductCatagoryCard/ProductCatagoryCard.js
@@ -8,13 +8,16 @@ const ProductCatagoryCard = ({ productCatagory }) => {
   //   const validImageUrl = image.startsWith("https://") ? image : "";
 
   return (
-    <div>
-      <div>
+    <div className="relative">
+      <div className="  flex flex-col items-center">
         <Image width={300} height={300} src={image} alt="image" />
-
-        <div>
-          <button>{category}</button>
-          <p>{products}</p>
+        <div className="px-4 absolute bottom-2 ">
+          <div className="border-2  items-center bg-transparent opacity-80 hover:bg-slate-100 text-center border-black px-1 bg-white">
+            <button className="text-[13px]  font-semibold">{category}</button>
+            <p className="text-gray-500 text-[12px]">
+              {products} <span>Products</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
