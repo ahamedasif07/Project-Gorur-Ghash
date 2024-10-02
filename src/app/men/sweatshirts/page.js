c763895daa0d6f501527be3d3a26cc0453196333
rxasif07 <rxasif31@gmail.com>
2024-10-02 22:51:40 +0600

    sweatshirts page done

---
+++ src/app/men/sweatshirts/page.js
@@ -0,0 +1,12 @@
+import SweatshirtsC from "@/components/SweatshirtsC/SweatshirtsC";
+import React from "react";
+
+const Sweatshirts = () => {
+  return (
+    <div>
+      <SweatshirtsC />
+    </div>
+  );
+};
+
+export default Sweatshirts;
--- src/app/men/t-shirts/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const tShirts = () => {
-  return <div>tShirts</div>;
-};
-
-export default tShirts;
