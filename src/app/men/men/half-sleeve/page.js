9bae257f8e1e8feedee042a85a9f511580c807a7
rxasif07 <rxasif31@gmail.com>
2024-10-07 21:38:32 +0700

    half sleevs shirts done

---
+++ src/app/men/men/half-sleeve/page.js
@@ -0,0 +1,12 @@
+import HalfSleevsC from "@/components/MenShirts/Half-Sleevs/HalfSleevsC";
+import React from "react";
+
+const HalfShirts = () => {
+  return (
+    <div>
+      <HalfSleevsC />
+    </div>
+  );
+};
+
+export default HalfShirts;
---
+++ src/components/MenShirts/Half-Sleevs/HalfSleevsC.js
@@ -0,0 +1,44 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { useDataContext } from "@/components/Shared/DataFetching/DataProvider";
+import DisplayCatagoryProducts from "@/components/Shared/DisplayCatagoryProducts";
+
+const HalfSleevsC = () => {
+  const [allProductsData, setAllProductsData] = useState([]);
+  const [halfSleevs] = useState("half-sleeve");
+  const [filtaredProducts, setFiltaredProducts] = useState([]);
+  const { data } = useDataContext(); // Assume this returns the data or null
+
+  // Use useEffect to update state only when data changes
+  useEffect(() => {
+    if (data) {
+      setAllProductsData(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (allProductsData.length > 0) {
+      const remaining = allProductsData.filter(
+        (product) =>
+          product.category.toLowerCase().trim() ===
+          halfSleevs.toLowerCase().trim()
+      );
+      setFiltaredProducts(remaining);
+    }
+  }, [allProductsData, halfSleevs]);
+
+  console.log("data from context", data);
+  console.log("all products", allProductsData);
+
+  return (
+    <div>
+      <DisplayCatagoryProducts
+        category="half-sleeve"
+        filtaredProducts={filtaredProducts}
+      ></DisplayCatagoryProducts>
+    </div>
+  );
+};
+
+export default HalfSleevsC;
