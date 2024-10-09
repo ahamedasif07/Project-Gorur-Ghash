140481930e2a0f521153a41bf0f83397a488448a
rxasif07 <rxasif31@gmail.com>
2024-10-09 08:27:44 +0600

    bandcoller page warning fix

--- src/app/men/men/band-collar/page.js
+++ src/app/men/men/band-collar/page.js
@@ -1,4 +1,4 @@
-import BandCollarC from "@/components/MenShirts/Band-collar/bandCollarC";
+import BandCollarC from "@/components/MenShirts/Band-collar/BandCollarC";
 import React from "react";
 
 const BandCollar = () => {
