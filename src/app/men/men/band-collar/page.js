a20a8b02ec5a90b02678c0a0bf6d46455577e0e9
rxasif07 <rxasif31@gmail.com>
2024-10-09 08:07:16 +0600

    band collor page done

---
+++ src/app/men/men/band-collar/page.js
@@ -0,0 +1,12 @@
+import BandCollarC from "@/components/MenShirts/Band-collar/bandCollarC";
+import React from "react";
+
+const BandCollar = () => {
+  return (
+    <div>
+      <BandCollarC />
+    </div>
+  );
+};
+
+export default BandCollar;
