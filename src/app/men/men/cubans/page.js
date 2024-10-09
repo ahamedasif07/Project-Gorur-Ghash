366065cc70640f50534a279abef84eacfbdb5073
rxasif07 <rxasif31@gmail.com>
2024-10-09 08:49:25 +0600

    cubans page done

---
+++ src/app/men/men/cubans/page.js
@@ -0,0 +1,12 @@
+import CubnasC from "@/components/MenShirts/Cubans/CubansC";
+import React from "react";
+
+const Cubans = () => {
+  return (
+    <div>
+      <CubnasC />
+    </div>
+  );
+};
+
+export default Cubans;
