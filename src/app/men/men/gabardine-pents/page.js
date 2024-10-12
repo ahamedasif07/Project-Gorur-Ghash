56e4419b6d9ae058688c5c47a8ca9dae2874adfb
rxasif07 <rxasif31@gmail.com>
2024-10-12 08:25:30 +0600

    gabardine-pants done

---
+++ src/app/men/men/gabardine-pents/page.js
@@ -0,0 +1,12 @@
+import GabardinPentsC from "@/components/men-pents/Gabardine-pents/GabardinePentsC";
+import React from "react";
+
+const GabadinePents = () => {
+  return (
+    <div>
+      <GabardinPentsC />
+    </div>
+  );
+};
+
+export default GabadinePents;
