765b733100bdf7e9929923170bb2dda959dea426
rxasif07 <rxasif31@gmail.com>
2024-10-13 07:58:16 +0600

    boxy jackets page done

---
+++ src/app/men/men/boxy-jacket/page.js
@@ -0,0 +1,12 @@
+import BoxyJacketsC from "@/components/Jackets/BoxyJacketsC/BoxyJacketsC";
+import React from "react";
+
+const BoxyJackets = () => {
+  return (
+    <div>
+      <BoxyJacketsC />
+    </div>
+  );
+};
+
+export default BoxyJackets;
