2e3595e53033dd30f00fc1883ffb2860ae8acff8
rxasif07 <rxasif31@gmail.com>
2024-10-13 09:16:03 +0600

    leather jacket done

---
+++ src/app/men/men/leather-jacket/page.js
@@ -0,0 +1,12 @@
+import LeatherJacketsC from "@/components/men-jackets/LeatherJacket/LeatherJacketC";
+import React from "react";
+
+const LeatherJacket = () => {
+  return (
+    <div>
+      <LeatherJacketsC />
+    </div>
+  );
+};
+
+export default LeatherJacket;
