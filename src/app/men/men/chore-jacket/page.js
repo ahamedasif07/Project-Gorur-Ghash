2926e4101c72c104acd37f2506571f919aeb71f1
rxasif07 <rxasif31@gmail.com>
2024-10-14 08:06:29 +0600

    chorge jacket page done

---
+++ src/app/men/men/chore-jacket/page.js
@@ -0,0 +1,12 @@
+import ChoreJacketsC from "@/components/men-jackets/ChoreJackets/ChoreJacketsC";
+import React from "react";
+
+const ChoreJackets = () => {
+  return (
+    <div>
+      <ChoreJacketsC />
+    </div>
+  );
+};
+
+export default ChoreJackets;
