194baa0cf12777a5c55700fd6157dd1383b62f56
rxasif07 <rxasif31@gmail.com>
2024-10-14 20:15:11 +0600

    mens jacket done

---
+++ src/app/men/men/harringtons-jacket/page.js
@@ -0,0 +1,12 @@
+import HarringtonsJacketC from "@/components/men-jackets/Harringtons-jacket/HarringtonsJacketC";
+import React from "react";
+
+const HarringtonsJacket = () => {
+  return (
+    <div>
+      <HarringtonsJacketC />
+    </div>
+  );
+};
+
+export default HarringtonsJacket;
