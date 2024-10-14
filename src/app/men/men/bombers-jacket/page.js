78b4ac7ad4a0ea427fbe6590b3d16bc96c802557
rxasif07 <rxasif31@gmail.com>
2024-10-14 08:01:33 +0600

    bomber jacket data macked

---
+++ src/app/men/men/bombers-jacket/page.js
@@ -0,0 +1,12 @@
+import BombersJacketC from "@/components/men-jackets/bombers-jacket/BombersJacketC";
+import React from "react";
+
+const BomberJackets = () => {
+  return (
+    <div>
+      <BombersJacketC />
+    </div>
+  );
+};
+
+export default BomberJackets;
