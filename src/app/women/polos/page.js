449dd2c337fadeb893d38550a8ee7628c955eaba
rxasif07 <rxasif31@gmail.com>
2024-10-17 07:54:26 +0600

    women polos data macked

---
+++ src/app/women/polos/page.js
@@ -0,0 +1,12 @@
+import PolosCW from "@/components/Women/Polos/PolosCW";
+import React from "react";
+
+const Polos = () => {
+  return (
+    <div>
+      <PolosCW />
+    </div>
+  );
+};
+
+export default Polos;
