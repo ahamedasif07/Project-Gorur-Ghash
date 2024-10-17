fde129f68971d7f0ad0dd974c55dfc4b7d3e7b6e
rxasif07 <rxasif31@gmail.com>
2024-10-17 08:17:54 +0600

    meta data set women catagoryes

--- src/app/women/cardigan%20+%20crop-top/page.js
+++ src/app/women/cardigan%20+%20crop-top/page.js
@@ -1,5 +1,10 @@
 import CardiganSetMeronCW from "@/components/Women/CardiganSetMeron/CardiganSetMeronCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | Cardingan-Top",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const CardinganTop = () => {
   return (
--- src/app/women/polos/page.js
+++ src/app/women/polos/page.js
@@ -1,6 +1,10 @@
 import PolosCW from "@/components/Women/Polos/PolosCW";
 import React from "react";
-
+export const metadata = {
+  title: "Gorur Ghash - women | polos",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 const Polos = () => {
   return (
     <div>
