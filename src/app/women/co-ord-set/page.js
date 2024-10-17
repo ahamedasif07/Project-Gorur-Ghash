7280f8c090adc24e90c957fe39d36aedefa952e4
rxasif07 <rxasif31@gmail.com>
2024-10-17 08:39:27 +0600

    skarts page done

--- src/app/women/co-ord-set/page.js
+++ src/app/women/co-ord-set/page.js
@@ -1,5 +1,10 @@
 import CoOrdSetCW from "@/components/Women/CoOrdSet/CoOrdSetCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | C0-Ord-Set",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const CoOrdSet = () => {
   return (
---
+++ src/app/women/skirts/page.js
@@ -0,0 +1,12 @@
+import SkirtsCW from "@/components/Women/Skirts/SkirtsCW";
+import React from "react";
+
+const Skirts = () => {
+  return (
+    <div>
+      <SkirtsCW />
+    </div>
+  );
+};
+
+export default Skirts;
