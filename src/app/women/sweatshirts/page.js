0719f9a205f4766687616874c53950d74ad5265c
rxasif07 <rxasif31@gmail.com>
2024-10-18 08:56:29 +0600

    sweetshirts data macked

--- src/app/women/sweatshirts/page.js
+++ src/app/women/sweatshirts/page.js
@@ -1,7 +1,17 @@
+import SweatshirtsCW from "@/components/Women/Sweatshirts/SweatshirtsCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | Sweatshirts",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const Sweatshirts = () => {
-  return <div>Sweatshirts</div>;
+  return (
+    <div>
+      <SweatshirtsCW />
+    </div>
+  );
 };
 
 export default Sweatshirts;
