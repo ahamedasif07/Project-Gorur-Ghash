addce85cf614a83974fa765c7383a61ab75135ab
rxasif07 <rxasif31@gmail.com>
2024-10-18 08:49:21 +0600

    sweatshirts women data maked

--- src/app/women/skirts/page.js
+++ src/app/women/skirts/page.js
@@ -1,5 +1,10 @@
 import SkirtsCW from "@/components/Women/Skirts/SkirtsCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | Skirts",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const Skirts = () => {
   return (
