e74a06395f22a8982e181410d14e189c5b779019
rxasif07 <rxasif31@gmail.com>
2024-10-19 08:51:37 +0600

    women-Rayon-Linen-Blend-Shirts page make

---
+++ src/app/women/women/rayon-linen-blend-shirts/page.js
@@ -0,0 +1,12 @@
+import RayonLinenBlensShirtsCW from "@/components/Women/women-shirts/Rayon-Linen-Blens-Shirts/RayonLinenBlensShirtsCW";
+import React from "react";
+
+const RayonLinenBlensShirts = () => {
+  return (
+    <div>
+      <RayonLinenBlensShirtsCW />
+    </div>
+  );
+};
+
+export default RayonLinenBlensShirts;
