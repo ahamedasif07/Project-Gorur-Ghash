48e6c7f0dc90d9f72419f8e582f1c5d6e9594c02
rxasif07 <rxasif31@gmail.com>
2024-10-23 09:17:07 +0600

    data maked of women drop teets

---
+++ src/app/women/women/basic-tees/page.js
@@ -0,0 +1,12 @@
+import BasicTeesCW from "@/components/Women/Women-TShirts/Basic-tees/BasicTshirtsCW";
+import React from "react";
+
+const BasicTees = () => {
+  return (
+    <div>
+      <BasicTeesCW />
+    </div>
+  );
+};
+
+export default BasicTees;
---
+++ src/app/women/women/drop-tees/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DropTees = () => {
+  return <div>DropTees</div>;
+};
+
+export default DropTees;
