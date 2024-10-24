411443545292be5fb1f611a1c8b098868ed87458
rxasif07 <rxasif31@gmail.com>
2024-10-24 09:03:33 +0600

    women tank tops data macked

---
+++ src/app/women/tank-top/page.js
@@ -0,0 +1,12 @@
+import WomenTankTopCW from "@/components/Women/women-tank-top/womenTankTopCW";
+import React from "react";
+
+const TankTops = () => {
+  return (
+    <div>
+      <WomenTankTopCW />
+    </div>
+  );
+};
+
+export default TankTops;
---
+++ src/components/Women/women-tank-top/womenTankTopCW.js
@@ -0,0 +1,44 @@
+// "use client";
+// import React, { useEffect, useState } from "react";
+
+// import { useDataContext } from "@/components/Shared/DataFetching/DataProvider";
+// import DisplayCatagoryProducts from "@/components/Shared/DisplayCatagoryProducts";
+
+// const WomenTankTopCW = () => {
+//   const [allProductsData, setAllProductsData] = useState([]);
+//   const [womenTankTop] = useState("women-tank-top");
+//   const [filtaredProducts, setFiltaredProducts] = useState([]);
+//   const { data } = useDataContext(); // Assume this returns the data or null
+
+//   // Use useEffect to update state only when data changes
+//   useEffect(() => {
+//     if (data) {
+//       setAllProductsData(data);
+//     }
+//   }, [data]);
+
+//   useEffect(() => {
+//     if (allProductsData.length > 0) {
+//       const remaining = allProductsData.filter(
+//         (product) =>
+//           product.category.toLowerCase().trim() ===
+//           womenTankTop.toLowerCase().trim()
+//       );
+//       setFiltaredProducts(remaining);
+//     }
+//   }, [allProductsData, womenTankTop]);
+
+//   console.log("data from context", data);
+//   console.log("all products", allProductsData);
+
+//   return (
+//     <div>
+//       <DisplayCatagoryProducts
+//         category="women-tank-top"
+//         filtaredProducts={filtaredProducts}
+//       ></DisplayCatagoryProducts>
+//     </div>
+//   );
+// };
+
+// export default WomenTankTopCW;
