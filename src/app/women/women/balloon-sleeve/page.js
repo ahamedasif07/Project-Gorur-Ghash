10f9d86592943559ea842419df69ce87c636db42
rxasif07 <rxasif31@gmail.com>
2024-10-25 09:35:58 +0600

    women women-Balloon-Sleeve page done

--- src/app/women/women/balloon-sleeve/page.js
+++ src/app/women/women/balloon-sleeve/page.js
@@ -1,7 +1,12 @@
+import BallonSleevsCW from "@/components/Women/Women-Tops/balloon-sleeve/BallonSleevsCW";
 import React from "react";
 
 const BallonSleevs = () => {
-  return <div>BallonSleevs</div>;
+  return (
+    <div>
+      <BallonSleevsCW />
+    </div>
+  );
 };
 
 export default BallonSleevs;
