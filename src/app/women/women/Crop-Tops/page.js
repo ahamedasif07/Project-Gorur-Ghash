964067c91a3e19d780c77f7818ff3f0ae6d170e6
rxasif07 <rxasif31@gmail.com>
2024-10-26 08:38:51 +0600

    crops tops data meked

--- src/app/women/women/Crop-Tops/page.js
+++ src/app/women/women/Crop-Tops/page.js
@@ -1,7 +1,12 @@
+import CropTopsCW from "@/components/Women/Women-Tops/CropTops/CropTopsCW";
 import React from "react";
 
 const CropTops = () => {
-  return <div>CropTops</div>;
+  return (
+    <div>
+      <CropTopsCW />
+    </div>
+  );
 };
 
 export default CropTops;
