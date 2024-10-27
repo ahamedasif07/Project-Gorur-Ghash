e69df3472f1693644829dd44b240596ba9076c90
rxasif07 <rxasif31@gmail.com>
2024-10-27 16:48:23 +0600

    wrap tops data page done

---
+++ src/app/women/women/wrap-tops/page.js
@@ -0,0 +1,12 @@
+import WrapTopsCW from "@/components/Women/Women-Tops/Wrap-Tops/WrapTopsCW";
+import React from "react";
+
+const WarpTops = () => {
+  return (
+    <div>
+      <WrapTopsCW />
+    </div>
+  );
+};
+
+export default WarpTops;
