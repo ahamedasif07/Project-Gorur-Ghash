22d443c0caaa7a2b7fa0651523c46880963882c6
rxasif07 <rxasif31@gmail.com>
2024-10-28 10:05:12 +0600

    women cargo data meked

--- src/app/women/pants/page.js
+++ src/app/women/pants/page.js
@@ -1,5 +1,10 @@
 import PantsCategoryCW from "@/components/Women/women-pents-catrgory/PentsCategoryCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | pents",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const Pants = () => {
   return (
---
+++ src/components/Women/women-pents/Cargos/Cargos.js
@@ -0,0 +1,48 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { useDataContext } from "@/components/Shared/DataFetching/DataProvider";
+import DisplayCatagoryProducts from "@/components/Shared/DisplayCatagoryProducts";
+
+const CargosCW = () => {
+  const [allProductsData, setAllProductsData] = useState([]);
+  const [Cargo] = useState("women-Cargo");
+  const [filtaredProducts, setFiltaredProducts] = useState([]);
+  const { data } = useDataContext(); // Assume this returns the data or null
+
+  // Use useEffect to update state only when data changes
+  useEffect(() => {
+    if (data) {
+      setAllProductsData(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (allProductsData.length > 0) {
+      const remaining = allProductsData.filter(
+        (product) =>
+          product.category.toLowerCase().trim() === Cargo.toLowerCase().trim()
+      );
+      setFiltaredProducts(remaining);
+    }
+  }, [allProductsData, Cargo]);
+
+  console.log("data from context", data);
+  console.log("all products", allProductsData);
+
+  return (
+    <div>
+      <DisplayCatagoryProducts
+        category="Cargo-women"
+        filtaredProducts={filtaredProducts}
+        description="Introducing Women’s Sweatshirts by Ghash, the perfect addition to your casual wardrobe! Made with high-quality, soft, and breathable fabric, these sweatshirts are designed to keep you warm and cozy on chilly days.
+
+Featuring a classic and versatile design, our sweatshirts are perfect for any occasion, whether you’re running errands, hitting the gym, or lounging at home. The ribbed cuffs and waistband provide a snug and secure fit, while the relaxed fit ensures maximum comfort.
+
+Our women’s sweats are available in a range of colors and sizes, so you can choose the perfect one to match your personal style. Whether you prefer a bold and vibrant hue or a subtle and classic shade, we’ve got you covered."
+      ></DisplayCatagoryProducts>
+    </div>
+  );
+};
+
+export default CargosCW;
