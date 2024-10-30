951f9b663bad8aa20c1f638908182838aec63716
rxasif07 <rxasif31@gmail.com>
2024-10-30 08:55:41 +0600

    women turtleneke data maked

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,7 @@ import LoadingC from "@/components/Loading/LoadingC";
 
 const loading = () => {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center h-[100vh] items-center">
       <LoadingC />
     </div>
   );
