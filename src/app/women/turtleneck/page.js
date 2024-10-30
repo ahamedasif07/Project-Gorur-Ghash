f1904b9b10ca3a7614c6cb8a91d4a5ac85f7e073
rxasif07 <rxasif31@gmail.com>
2024-10-30 09:26:29 +0600

    hoddis data maked

--- src/app/women/turtleneck/page.js
+++ src/app/women/turtleneck/page.js
@@ -1,5 +1,10 @@
 import TurtleneckCW from "@/components/Women/Turtleneck/TurtleneckCW";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | Turtleneck",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const Turtleneck = () => {
   return (
