e5b06005662a0f6011b02c0e246ed26b53bd7ee8
rxasif07 <rxasif31@gmail.com>
2024-10-30 09:35:33 +0600

    hoddie page done

---
+++ src/app/women/hoodie-blanket/page.js
@@ -0,0 +1,17 @@
+import HoodieBlanketC from "@/components/HoodieBlanket/HoodieBlanketC";
+import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | HoddiBlancket",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
+
+const HoddieBlancket = () => {
+  return (
+    <div>
+      <HoodieBlanketC />
+    </div>
+  );
+};
+
+export default HoddieBlancket;
---
+++ src/app/women/hoodie/page.js
@@ -0,0 +1,16 @@
+import HoddieCW from "@/components/Women/Hoddie/HoddieCW";
+import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - women | Hoddie",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
+const Hoddie = () => {
+  return (
+    <div>
+      <HoddieCW />
+    </div>
+  );
+};
+
+export default Hoddie;
