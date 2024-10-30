0792aabba7f9a9afe592e71e9fea0466d1f1f155
rxasif07 <rxasif31@gmail.com>
2024-10-30 09:03:05 +0600

    women Turtleneck page done

---
+++ src/app/women/turtleneck/page.js
@@ -0,0 +1,12 @@
+import TurtleneckCW from "@/components/Women/Turtleneck/TurtleneckCW";
+import React from "react";
+
+const Turtleneck = () => {
+  return (
+    <div>
+      <TurtleneckCW />
+    </div>
+  );
+};
+
+export default Turtleneck;
