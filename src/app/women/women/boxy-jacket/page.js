00ba1aae255ffd14a1c5eee490f49c463dc33d05
rxasif07 <rxasif31@gmail.com>
2024-11-02 08:27:00 +0600

    women boxy jackets page done

--- src/app/women/women/boxy-jacket/page.js
+++ src/app/women/women/boxy-jacket/page.js
@@ -1,7 +1,12 @@
+import BoxyJacketsCW from "@/components/Women/Women-Jackets/BoxyJackets/BoxyJacketsCW";
 import React from "react";
 
 const BoxyJacket = () => {
-  return <div>BoxyJacket</div>;
+  return (
+    <div>
+      <BoxyJacketsCW />
+    </div>
+  );
 };
 
 export default BoxyJacket;
---
+++ src/components/Women/Women-Jackets/BoxyJackets/BoxyJacketsCW.js
@@ -0,0 +1,49 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import { useDataContext } from "@/components/Shared/DataFetching/DataProvider";
+import DisplayCatagoryProducts from "@/components/Shared/DisplayCatagoryProducts";
+
+const BoxyJacketsCW = () => {
+  const [allProductsData, setAllProductsData] = useState([]);
+  const [BoxyJackets] = useState("women-boxy-jacket");
+  const [filtaredProducts, setFiltaredProducts] = useState([]);
+  const { data } = useDataContext(); // Assume this returns the data or null
+
+  // Use useEffect to update state only when data changes
+  useEffect(() => {
+    if (data) {
+      setAllProductsData(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (allProductsData.length > 0) {
+      const remaining = allProductsData.filter(
+        (product) =>
+          product.category.toLowerCase().trim() ===
+          BoxyJackets.toLowerCase().trim()
+      );
+      setFiltaredProducts(remaining);
+    }
+  }, [allProductsData, BoxyJackets]);
+
+  console.log("data from context", data);
+  console.log("all products", allProductsData);
+
+  return (
+    <div>
+      <DisplayCatagoryProducts
+        category="Boxy Jackets-women"
+        filtaredProducts={filtaredProducts}
+        description="Introducing Women’s BoxyJackets by Ghash, the perfect addition to your casual wardrobe! Made with high-quality, soft, and breathable fabric, these BoxyJackets are designed to keep you warm and cozy on chilly days.
+
+Featuring a classic and versatile design, our BoxyJackets are perfect for any occasion, whether you’re running errands, hitting the gym, or lounging at home. The ribbed cuffs and waistband provide a snug and secure fit, while the relaxed fit ensures maximum comfort.
+
+Our women’s sweats are available in a range of colors and sizes, so you can choose the perfect one to match your personal style. Whether you prefer a bold and vibrant hue or a subtle and classic shade, we’ve got you covered."
+      ></DisplayCatagoryProducts>
+    </div>
+  );
+};
+
+export default BoxyJacketsCW;
