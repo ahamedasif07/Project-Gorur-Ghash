d9f83c12201e15d8a1bc499141eb724cb2f60bf3
rxasif07 <rxasif31@gmail.com>
2024-11-03 09:31:59 +0600

    women bomber jacket page maked

---
+++ src/app/women/women/bombers/page.js
@@ -0,0 +1,12 @@
+import BomberJacketCW from "@/components/Women/Women-Jackets/Bomber-Jacket/BomberJacketCW";
+import React from "react";
+
+const BomberJacket = () => {
+  return (
+    <div>
+      <BomberJacketCW />
+    </div>
+  );
+};
+
+export default BomberJacket;
