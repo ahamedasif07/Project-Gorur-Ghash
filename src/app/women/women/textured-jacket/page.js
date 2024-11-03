9857a5d99108f62c260813ce866a88bdab1f8971
rxasif07 <rxasif31@gmail.com>
2024-11-03 08:56:52 +0600

    women textured jackets data maked done

---
+++ src/app/women/women/textured-jacket/page.js
@@ -0,0 +1,12 @@
+import TexturedJacketCW from "@/components/Women/Women-Jackets/Textured-Jacket/TexturedJacketCW";
+import React from "react";
+
+const TexturedJacket = () => {
+  return (
+    <div>
+      <TexturedJacketCW />
+    </div>
+  );
+};
+
+export default TexturedJacket;
