2c28021efd6cf33aaac3e49d8efae7fa7094b610
rxasif07 <rxasif31@gmail.com>
2024-11-04 07:47:14 +0600

    women chore jackets page maked

---
+++ src/app/women/women/chore/page.js
@@ -0,0 +1,12 @@
+import ChoreJacketsCW from "@/components/Women/Women-Jackets/Chore-Jacket/ChoreJacketCW";
+import React from "react";
+
+const Chore = () => {
+  return (
+    <div>
+      <ChoreJacketsCW />
+    </div>
+  );
+};
+
+export default Chore;
