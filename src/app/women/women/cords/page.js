b20a407f77b5c0d717d62a4b797286cd2555fe69
rxasif07 <rxasif31@gmail.com>
2024-11-04 08:10:15 +0600

    women cord jackets page done

--- src/app/women/women/cords/page.js
+++ src/app/women/women/cords/page.js
@@ -1,7 +1,12 @@
+import CordJacketsCW from "@/components/Women/Women-Jackets/Cord-jacket/CordJacketCW";
 import React from "react";
 
 const Cords = () => {
-  return <div>Cords</div>;
+  return (
+    <div>
+      <CordJacketsCW />
+    </div>
+  );
 };
 
 export default Cords;
