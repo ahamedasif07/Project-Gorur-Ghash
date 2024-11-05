fc50a1b364cf9483f9b8e3bbbc85dba5719737c7
rxasif07 <rxasif31@gmail.com>
2024-11-05 09:43:18 +0600

    kids jackets page done

---
+++ src/app/kids/jackets/page.js
@@ -0,0 +1,12 @@
+import JacketsKD from "@/components/Kids/Jackets/JacketsKD";
+import React from "react";
+
+const Jackets = () => {
+  return (
+    <div>
+      <JacketsKD />
+    </div>
+  );
+};
+
+export default Jackets;
