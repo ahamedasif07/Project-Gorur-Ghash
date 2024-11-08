0bae9515b926ff70369a18d05a0b865e13c570c2
rxasif07 <rxasif31@gmail.com>
2024-11-08 08:42:27 +0600

    military caps page maked

---
+++ src/app/accessories/accessories/military-cap/page.js
@@ -0,0 +1,12 @@
+import MilitaryCapC from "@/components/Accessories/caps/Military-Caps/MilitaryCapsC";
+import React from "react";
+
+const MillitaryCap = () => {
+  return (
+    <div>
+      <MilitaryCapC />
+    </div>
+  );
+};
+
+export default MillitaryCap;
