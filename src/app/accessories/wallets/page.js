4152582522666e2c5e4328d3ecde3f4b4dab5ea8
rxasif07 <rxasif31@gmail.com>
2024-11-11 21:38:35 +0600

    wallate page maked

---
+++ src/app/accessories/wallets/page.js
@@ -0,0 +1,16 @@
+import FilterByCategory from "@/components/Shared/FilterByCategory";
+import React from "react";
+
+const Wallets = () => {
+  return (
+    <div>
+      <FilterByCategory
+        category="wallet"
+        CategoryTitle="wallet"
+        description=""
+      ></FilterByCategory>
+    </div>
+  );
+};
+
+export default Wallets;
