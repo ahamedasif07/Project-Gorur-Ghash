74b8f3fd3c9cad53c11aaae2d705af857fa4456b
rxasif07 <rxasif31@gmail.com>
2024-11-13 08:32:10 +0600

    ear cuffes page maked

--- src/app/accessories/accessories/ear-cuffs/page.js
+++ src/app/accessories/accessories/ear-cuffs/page.js
@@ -1,7 +1,20 @@
+import FilterByCategory from "@/components/Shared/FilterByCategory";
 import React from "react";
+export const metadata = {
+  title: "Gorur Ghash - Accessories | Chain",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
+};
 
 const EarCuffs = () => {
-  return <div>EarCuffs</div>;
+  return (
+    <div>
+      <FilterByCategory
+        category="Ear-Cuffs"
+        CategoryTitle="EarCuffs"
+      ></FilterByCategory>
+    </div>
+  );
 };
 
 export default EarCuffs;
