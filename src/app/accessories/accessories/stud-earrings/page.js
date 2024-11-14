8e7e30629ed04d5cab59ba0d07d6b9ef2f8fad36
rxasif07 <rxasif31@gmail.com>
2024-11-14 11:07:38 +0600

    women ear rings data maked

---
+++ src/app/accessories/accessories/stud-earrings/page.js
@@ -0,0 +1,16 @@
+import FilterByCategory from "@/components/Shared/FilterByCategory";
+import React from "react";
+
+const StudEarrings = () => {
+  return (
+    <div>
+      <FilterByCategory
+        category="Stud-earrings"
+        CategoryTitle="Stud-earrings"
+        description="Need funky socks? Gorur Ghash has got you covered! Our collection of socks is all about vibrant colors and bold patterns that stand out. Whether you’re looking to brighten up your day or add a quirky touch to your outfit, these funky socks are the perfect choice. Designed to be playful and unique, they let you express your personality with every step. From eye-catching stripes to crazy prints, there’s a pair for every mood."
+      ></FilterByCategory>
+    </div>
+  );
+};
+
+export default StudEarrings;
