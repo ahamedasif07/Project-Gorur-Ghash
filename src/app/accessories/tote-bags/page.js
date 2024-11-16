214c083c2ce817098a9b1ff3ac06fb479ebba9f5
rxasif07 <rxasif31@gmail.com>
2024-11-16 08:54:50 +0600

    tote bags data page maked

--- src/app/accessories/tote-bags/page.js
+++ src/app/accessories/tote-bags/page.js
@@ -1,7 +1,21 @@
+import FilterByCategory from "@/components/Shared/FilterByCategory";
 import React from "react";
-
-const ToteBAges = () => {
-  return <div>ToteBAges</div>;
+export const metadata = {
+  title: "Gorur Ghash - accessories | wallet",
+  description:
+    "Welcome to the Gorur Ghash homepage where you can explore the latest fashion for men and women.",
 };
 
-export default ToteBAges;
+const ToteBags = () => {
+  return (
+    <div>
+      <FilterByCategory
+        category="tote-bags"
+        CategoryTitle="Tote-bags"
+        description=""
+      ></FilterByCategory>
+    </div>
+  );
+};
+
+export default ToteBags;
