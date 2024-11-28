56819701b6a0ac96ffe957b6f7eb537b3886c409
rxasif07 <rxasif31@gmail.com>
2024-11-28 18:02:40 +0600

    image sets

--- src/app/policies/refund-returns/page.js
+++ src/app/policies/refund-returns/page.js
@@ -1,7 +1,21 @@
 import React from "react";
+import exchangePolice from "../../../asstes/gorurghash-x-cheez/ep.png";
+import retrunPolice from "../../../asstes/gorurghash-x-cheez/rp.png";
+import tilePolice from "../../../asstes/gorurghash-x-cheez/tp.png";
+import Image from "next/image";
 
 const Return = () => {
-  return <div>Return</div>;
+  return (
+    <div>
+      <div className="max-w-[1100px] mx-auto flex  gap-6 mt-4 ">
+        <Image src={exchangePolice} alt="image"></Image>
+        <Image src={retrunPolice} alt="image"></Image>
+      </div>
+      <div className="flex justify-center mt-4">
+        <Image src={tilePolice} alt="image"></Image>
+      </div>
+    </div>
+  );
 };
 
 export default Return;
