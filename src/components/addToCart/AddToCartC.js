9a950d9f1713744dd3cc5e9dec5d010d0f2722c0
rxasif07 <rxasif31@gmail.com>
2024-11-29 16:17:52 +0600

    almost adding funcionality done

--- src/components/addToCart/AddToCartC.js
+++ src/components/addToCart/AddToCartC.js
@@ -6,18 +6,21 @@ import AddToCartCard from "./addToCartCard/addToCardCard";
 const AddToCart = () => {
   const { addToCart } = useCartContext();
   return (
-    <div>
-      <div className="pt-16">
-        <h2 className="text-4xl text-center">Cart</h2>
+    <div className="max-w-3xl mx-auto">
+      <div className="pt-16 ">
+        <h2 className="text-5xl font-bold text-center">Cart</h2>
         <p className="w-full h-[3px] my-3 bg-gray-700 "></p>
       </div>
-      <div className="py-4 my-10">
+      <div className="py-4 mt-10 mb-4 ">
         {addToCart.map((product) => (
           <div key={product.id} className="py-5">
             <AddToCartCard product={product}></AddToCartCard>
           </div>
         ))}
       </div>
+      <button className="px-6 py-2  bg-yellow-400 text-white rounded-md hover:bg-yellow-500">
+        Buy Now
+      </button>
     </div>
   );
 };
