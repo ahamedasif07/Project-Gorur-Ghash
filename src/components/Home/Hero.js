d18e21ff6c9657cf958c2c3e3df536d0e930f305
rxasif07 <rxasif31@gmail.com>
2024-12-04 09:14:09 +0600

    home page slider maked

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -1,11 +1,32 @@
-import Image from "next/image";
-import heroImg from "@/asstes/gorur-ghash-home/GG-Puja-SSL2.png";
+import Slider from "../Shared/slider/Slider";
 
 const Hero = () => {
+  const sliderData = [
+    {
+      id: 1,
+      name: "Mountain View",
+      description: "A beautiful mountain landscape during sunrise.",
+      image:
+        "https://i.ibb.co.com/q9LMMkj/Colorful-Illustrated-Online-Business-and-E-Commerce-Blog-Banner-2600-x-1260-px-1.png",
+    },
+    {
+      id: 2,
+      name: "Ocean Bliss",
+      description: "A serene ocean view with a setting sun.",
+      image: "https://i.ibb.co.com/LQftmMp/Winter-wear.png",
+    },
+    {
+      id: 3,
+      name: "Forest Retreat",
+      description:
+        "A peaceful forest with sunlight filtering through the trees.",
+      image: "https://i.ibb.co.com/VWswqkC/5-3.png",
+    },
+  ];
   return (
     <div>
-      <div className="md:pt-[110px] max-w-[1200px] max-h-[680px] mx-auto pt-[30px] ">
-        <Image className="" src={heroImg} alt="image"></Image>
+      <div className="md:pt-[60px] md:px-12 px-4 max-h-[700px] mx-auto pt-[30px] ">
+        <Slider slides={sliderData}></Slider>
       </div>
     </div>
   );
