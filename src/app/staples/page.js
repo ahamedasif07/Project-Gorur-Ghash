29939be58b6394880f76f12f3b279846c9e1c645
rxasif07 <rxasif31@gmail.com>
2024-12-11 14:58:24 +0600

    steplers page maked

---
+++ src/app/staples/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Staples = () => {
+  return (
+    <div className="text-5xl text-center font-bold py-7">Noting Avalable</div>
+  );
+};
+
+export default Staples;
