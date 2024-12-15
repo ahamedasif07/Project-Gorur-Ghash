17e5d79dffa6f4970bb2798bb31daa99fb64009e
rxasif07 <rxasif31@gmail.com>
2024-12-15 09:54:37 +0600

    scarch funcionality sone

--- src/components/ScarchFildeC/ScarchFildeC.js
+++ src/components/ScarchFildeC/ScarchFildeC.js
@@ -1,13 +1,39 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSearchContext } from "../ScarchContex/ScarchContex";
+import { useDataContext } from "../Shared/DataFetching/DataProvider";
+import ProductCard from "../Shared/ProductCard";
 
 const ScarchFildeC = () => {
   const { inputValue } = useSearchContext();
   console.log(inputValue);
+  const { data } = useDataContext();
+  const [searchItems, setSearchItems] = useState([]);
+
+  console.log(data);
+  console.log(inputValue);
+
+  useEffect(() => {
+    if (data && inputValue) {
+      const searchCategory = data.filter(
+        (product) =>
+          product.category.toLowerCase().trim() ===
+          inputValue.toLowerCase().trim()
+      );
+      setSearchItems(searchCategory);
+    } else {
+      setSearchItems([]); // Clear the list if no data or inputValue
+    }
+  }, [inputValue, data]);
+
   return (
     <div>
-      <p>{inputValue || "No input yet"}</p>
+      <div className="grid grid-cols-4">
+        {searchItems.map((product) => (
+          <ProductCard key={product.id} product={product}></ProductCard>
+        ))}
+      </div>
     </div>
   );
 };
